fc5d1120c3c645a254ec4c1011b61b7ffd8d1784
Ashwani <you@example.com>
2026-04-26 11:46:09 +0530

    api connected

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:8181/api",
+  baseURL: "https://quiz-backend-j355.onrender.com/api",
 });
 
 axiosInstance.interceptors.request.use(function (config) {
